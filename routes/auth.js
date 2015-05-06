9dda23049e3be4c9fcdef87b2185b1011df1761b
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-05-06 14:48:46 +0900

    refactor routes/auth

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
 
 // third party
 var config = require('config');
+var async = require('async');
 
 //dao
 var userDao = require('models/dao/user_dao');
@@ -49,55 +50,59 @@ exports.init = function(router) {
     router.get('/auth/facebook', passport.authenticate('facebook-canvas', { scope: ['email'] } ));
 
     //　facebook認証が終わると、このoauthloginにget通信がされる。
-    router.get('/auth/facebook/callback', passport.authenticate('facebook-canvas', {failureRedirect: '/login?fail'}),
-        function(request, response) {
-            userDao.getByFacebookId(request.user.id, function(error, result) {
-                if(error) {
-                    errorHandler.invalidrequestuest(response, error);
-                    return;
-                }
+    router.get('/auth/facebook/callback', passport.authenticate('facebook-canvas', {failureRedirect: '/auth'}), function(request, response) {
+        async.waterfall([
+            function(callback) {
+                userDao.getByFacebookId(request.user.id, callback);
+            },
+            function(result, callback) {
                 // ログアウトしていた場合
                 if (result && result.deleteFlag === false) {
-                    request.session.userId = result.id;
-                    request.session.accessToken = passport.session.accessToken;
-                    response.redirect('/');
-                // アカウント削除していた場合
-                } else if (result && result.deleteFlag) {
-                    userDao.updateByUserId(result.userId, {deleteFlag: false}, function(error) {
-                        if (error) {
-                            errorHandler.invalidrequestuest(response, error);
-                            return;
-                        }
-                        request.session.userId = result.id;
-                        response.redirect('/');
-                    });
-                // 新規アカウント作成
-                } else {
-                    var user = request.user;
-                    var data = {
-                        facebookId: user.id,
-                        username: user.displayName,
-                        gender: user.gender,
-                        email: user.emails[0].value,
-                        imagePath: 'https://graph.facebook.com/' + user.id + '/picture'
-                    }
-                    userDao.add(data, function(error, data) {
-                        if(error) {
-                            errorHandler.invalidrequestuest(response, error);
-                            return;
-                        }
-                        request.session.userId = data.id;
-
-                        if(data.email){
-                            // 登録完了メール送信
-                            // mailer.sendWellcome(data.email, user.displayName);
-                        }
-                        response.redirect('/');
-                    });
+                    callback(null, result.id);
+                    return;
                 }
-            });
-        }
-    );
+
+                // アカウント削除していた場合
+                if (result && result.deleteFlag) {
+                    userDao.updateByUserId({
+                        id: result.id,
+                        uodateParameter: {
+                            deleteFlag: false
+                        }
+                    }, function(error) {
+                        if (error) {
+                            callback(error);
+                            return;
+                        }
+                        callback(null, result.id);
+                    });
+                    return;
+                }
+
+                var user = request.user;
+                userDao.add({
+                    facebookId: user.id,
+                    username: user.displayName,
+                    gender: user.gender,
+                    email: user.emails[0].value,
+                    imagePath: 'https://graph.facebook.com/' + user.id + '/picture'
+                }, function(error, data) {
+                    if(error) {
+                        callback(error);
+                        return;
+                    }
+                    callback(null, data.id);
+                });
+            }
+        ], function(error, userId) {
+            if (error) {
+                errorHandler.invalidrequestuest(response, error);
+                return;
+            }
+            request.session.userId = userId;
+            response.redirect('/');
+        });
+    });
 
     // passport.use(new TwitterStrategy({
     //         consumerKey: app.get('config').twitter.consumer_key,
