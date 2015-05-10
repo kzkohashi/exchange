c0ea2a6d184067417313d13bf329ec33bdf23fe0
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-05-11 01:47:59 +0900

    modified user page

--- routes/user.js
+++ routes/user.js
@@ -11,8 +11,37 @@ var userFacade = require('models/facade/user_facade');
 exports.init = function(router) {
     /* GET home page. */
     router.get('/user/:userPageId', function(request, response) {
+
+        // validation
+        if (request.param('offset')) {
+            request.assert('offset').isInt();
+        }
+        if (request.param('limit')) {
+            request.assert('limit').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var offset = 0;
+        if (request.param('offset')) {
+            request.sanitize('offset').toInt();
+            offset = request.param('offset')
+        }
+        var limit = 12;
+        if (request.param('limit')) {
+            request.sanitize('limit').toInt();
+            limit = request.param('limit')
+        }
+
         userFacade.index({
             userId: request.session.userId,
+            offset: offset,
+            limit: limit,
             userPageId: parseInt(request.param('userPageId'))
         }, function(error, result) {
             if (error) {
