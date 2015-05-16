2179d8ab187b482c51c8fe1c081be45a46e7a7e1
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-05-17 01:58:22 +0900

    add comment_service

--- routes/goods.js
+++ routes/goods.js
@@ -16,6 +16,57 @@ exports.init = function(router) {
         if (request.param('userGoodsId')) {
             request.assert('userGoodsId').isInt();
         }
+        if (request.param('offset')) {
+            request.assert('offset').isInt();
+        }
+        if (request.param('limit')) {
+            request.assert('limit').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var userGoodsId = null;
+        if (request.param('userGoodsId')) {
+            request.sanitize('userGoodsId').toInt();
+            userGoodsId = request.param('userGoodsId');
+        }
+        var offset = 0;
+        if (request.param('offset')) {
+            request.sanitize('offset').toInt();
+            offset = request.param('offset')
+        }
+        var limit = 12;
+        if (request.param('limit')) {
+            request.sanitize('limit').toInt();
+            limit = request.param('limit')
+        }
+
+        goodsFacade.index({
+            userId: request.session.userId,
+            userGoodsId: userGoodsId,
+            offset: offset,
+            limit: limit,
+            currentDatetime: request.currentDatetime
+        }, function(error, result) {
+            if (error) {
+                errorHandler.index(response, error);
+                return;
+            }
+            response.render('goods/index', result);
+        });
+    });
+
+    router.post('/goods/comment', function(request, response) {
+
+        // validation
+        if (request.param('userGoodsId')) {
+            request.assert('userGoodsId').isInt();
+        }
 
         var errors = request.validationErrors();
         if (errors) {
@@ -30,16 +81,17 @@ exports.init = function(router) {
             userGoodsId = request.param('userGoodsId');
         }
 
-        goodsFacade.index({
-          userId: request.session.userId,
-          userGoodsId: userGoodsId,
-          currentDatetime: request.currentDatetime
+        goodsFacade.comment({
+            userId: request.session.userId,
+            userGoodsId: userGoodsId,
+            comment: request.param('comment'),
+            currentDatetime: request.currentDatetime
         }, function(error, result) {
             if (error) {
                 errorHandler.index(response, error);
                 return;
             }
-            response.render('goods/index', result);
+            response.redirect('/goods?userGoodsId=' + userGoodsId);
         });
     });
 }
--- routes/upload.js
+++ routes/upload.js
@@ -9,31 +9,57 @@ var errorHandler = require('errors/error');
 var uploadFacade = require('models/facade/upload_facade');
 
 exports.init = function(router) {
-    /* GET home page. */
     router.get('/upload', function(request, response) {
         uploadFacade.index({
-          userId: request.session.userId
+            userId: request.session.userId
         }, function(error, result) {
             if (error) {
                 errorHandler.invalidRequest(response, error);
                 return;
             }
-            console.log(result);
             response.render('upload/index', result);
         });
     });
 
     router.post('/upload/execute', function(request, response) {
 
-        var data = request.body;
-        var description = data.description;
-        var price = parseInt(data.price);
-        var brandId = parseInt(data.brandId);
-        var bagTypeId = parseInt(data.bagTypeId);
+        // validation
+        if (request.param('price')) {
+            request.assert('price').isInt();
+        }
+        if (request.param('brandId')) {
+            request.assert('brandId').isInt();
+        }
+        if (request.param('bagTypeId')) {
+            request.assert('bagTypeId').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var price = null;
+        if (request.param('price')) {
+            request.sanitize('price').toInt();
+            price = request.param('price');
+        }
+        var brandId = null;
+        if (request.param('brandId')) {
+            request.sanitize('brandId').toInt();
+            brandId = request.param('brandId');
+        }
+        var bagTypeId = null;
+        if (request.param('bagTypeId')) {
+            request.sanitize('bagTypeId').toInt();
+            bagTypeId = request.param('bagTypeId');
+        }
 
         uploadFacade.execute({
             userId: request.session.userId,
-            description: description,
+            description: request.param('description'),
             price: price,
             brandId: brandId,
             bagTypeId: bagTypeId,
