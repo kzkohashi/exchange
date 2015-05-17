5f914244d70c6cb475185405335c0ddeab91e9fe
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-05-17 22:53:53 +0900

    refactro login and add auth_facade

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,9 @@ var async = require('async');
 //dao
 var userDao = require('models/dao/user_dao');
 
+// facade
+var authFacade = require('models/facade/auth_facade');
+
 // error handler
 var errorHandler = require('errors/error');
 
@@ -16,6 +19,26 @@ var errorHandler = require('errors/error');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook-canvas');
 var TwitterStrategy = require('passport-twitter').Strategy;
+
+//OAuth認証用
+passport.use(new FacebookStrategy({
+    clientID: config.get('facebook.appId'),
+    clientSecret: config.get('facebook.appSecret'),
+    callbackURL: config.get('facebook.callbackUrl'),
+}, function(accessToken, refresponsehToken, profile, callback){
+    passport.session.accessToken = accessToken;
+    process.nextTick(function(){
+        callback(null, profile);
+    });
+}));
+
+passport.serializeUser(function(user, callback){
+    callback(null, user);
+});
+
+passport.deserializeUser(function(obj, callback){
+    callback(null, obj);
+});
 
 exports.init = function(router) {
 
@@ -28,82 +51,25 @@ exports.init = function(router) {
         response.redirect('/auth');
     });
 
-    //OAuth認証用
-    passport.use(new FacebookStrategy({
-        clientID: config.get('facebook.appId'),
-        clientSecret: config.get('facebook.appSecret'),
-        callbackURL: config.get('facebook.callbackUrl'),
-    }, function(accessToken, refresponsehToken, profile, callback){
-        passport.session.accessToken = accessToken;
-        process.nextTick(function(){
-            callback(null, profile);
-        });
-    }));
-
-    passport.serializeUser(function(user, callback){
-        callback(null, user);
-    });
-
-    passport.deserializeUser(function(obj, callback){
-        callback(null, obj);
-    });
-
     // /authにアクセスする事で、facebook認証につながる。
     router.get('/auth/facebook', passport.authenticate('facebook-canvas', { scope: ['email'] } ));
 
     //　facebook認証が終わると、このoauthloginにget通信がされる。
     router.get('/auth/facebook/callback', passport.authenticate('facebook-canvas', {failureRedirect: '/auth'}), function(request, response) {
-        async.waterfall([
-            function(callback) {
-                userDao.getByFacebookId(request.user.id, callback);
-            },
-            function(result, callback) {
-                // ログアウトしていた場合
-                if (result && result.deleteFlag === false) {
-                    callback(null, result.id);
-                    return;
-                }
 
-                // アカウント削除していた場合
-                if (result && result.deleteFlag) {
-                    userDao.updateByUserId({
-                        id: result.id,
-                        uodateParameter: {
-                            deleteFlag: false
-                        }
-                    }, function(error) {
-                        if (error) {
-                            callback(error);
-                            return;
-                        }
-                        callback(null, result.id);
-                    });
-                    return;
-                }
-
-                var user = request.user;
-                userDao.add({
-                    facebookId: user.id,
-                    username: user.displayName,
-                    gender: user.gender,
-                    email: user.emails[0].value,
-                    imagePath: 'https://graph.facebook.com/' + user.id + '/picture'
-                }, function(error, data) {
-                    if(error) {
-                        callback(error);
-                        return;
-                    }
-                    callback(null, data.id);
-                });
-            }
-        ], function(error, userId) {
+        authFacade.facebookCallback({
+            facebookId: request.user.id,
+            username: request.user.displayName,
+            gender: request.user.gender,
+            email: request.user.emails[0].value,
+            imagePath: 'https://graph.facebook.com/' + request.user.id + '/picture',
+            currentDatetime: request.currentDatetime
+        }, function(error, result) {
             if (error) {
                 errorHandler.invalidRequest(response, error);
                 return;
             }
-            console.log('99999999999999')
-            console.log(request.session);
-            request.session.userId = userId;
+            request.session.userId = result.userId;
             response.redirect('/');
         });
     });
