d0f1c5c2603ddb93dc2973ea13c6558d3f620f91
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-06-01 21:44:28 +0900

    base_setting.js

---
+++ public/javascripts/config/base_setting.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//configure
+var configure = {
+    _config : {},
+    add: function(key, val){
+        if(this._config[key]){
+            throw new Error('Configure Error: The key [' + key + '] is already exists.');
+        }
+        this._config[key] = val;
+    },
+    mod: function(key, val){
+        if(this._config[key] == undefined){
+            throw new Error('Configure Error: The key [' + key + '] is not exists.');
+        }
+        this._config[key] = val;
+    },
+    get: function(key){
+        return this._config[key];
+    }
+};
+
+configure.add('requirejsSettings',
+    {
+        baseUrl: '/javascripts/lib',
+        paths: {
+            'backbone': 'backbone-min',
+            'bootstrap': 'bootstrap.min',
+            'jquery': 'jquery.min',
+            'underscore': 'underscore-min'
+        },
+        // moduleの依存関係
+        shim: {
+            'backbone': {
+                deps: ['underscore', 'jquery'],
+                exports: 'Backbone'
+            }
+        }
+    }
+);
+
+if(typeof requirejs == 'object'){
+    requirejs.config(configure.get('requirejsSettings'));
+}
+
+//for node.js
+if(typeof exports != 'undefined'){
+    module.exports = {
+        configure: configure,
+    }
+}
