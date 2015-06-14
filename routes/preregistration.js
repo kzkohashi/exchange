84e25a0674d63a7cec23bc5028d72dc0a61b30c7
tomokazukozuma <tomokazu.kozuma@gmail.com>
2015-06-14 17:30:52 +0900

    change file name

---
+++ routes/preregistration.js
@@ -0,0 +1,135 @@
+/**
+ * @fileOverview top page
+ */
+
+// error handler
+var errorHandler = require('errors/error');
+
+// facade
+var preregistrationFacade = require('models/facade/preregistraion_facade');
+
+exports.init = function(router) {
+
+    router.get('/preregistration', function(request, response) {
+
+        // validation
+        if (request.param('offset')) {
+            request.assert('offset').isInt();
+        }
+        if (request.param('limit')) {
+            request.assert('limit').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var offset = 0;
+        if (request.param('offset')) {
+            request.sanitize('offset').toInt();
+            offset = request.param('offset')
+        }
+        var limit = 20;
+        if (request.param('limit')) {
+            request.sanitize('limit').toInt();
+            limit = request.param('limit')
+        }
+
+        preregistrationFacade.index({
+            offset: offset,
+            limit: limit,
+            currentDatetime: request.currentDatetime
+        }, function(error, result) {
+            if (error) {
+                errorHandler.index(response, error);
+                return;
+            }
+            response.render('registration/index', result);
+        });
+    });
+
+    router.get('/preregistrationFacade/execute', function(request, response) {
+
+        // validation
+        if (request.param('offset')) {
+            request.assert('offset').isInt();
+        }
+        if (request.param('limit')) {
+            request.assert('limit').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var offset = 0;
+        if (request.param('offset')) {
+            request.sanitize('offset').toInt();
+            offset = request.param('offset')
+        }
+        var limit = 20;
+        if (request.param('limit')) {
+            request.sanitize('limit').toInt();
+            limit = request.param('limit')
+        }
+
+        preregistrationFacade.execute({
+            offset: offset,
+            limit: limit,
+            currentDatetime: request.currentDatetime
+        }, function(error, result) {
+            if (error) {
+                errorHandler.index(response, error);
+                return;
+            }
+            response.render('preregistrationFacade/index', result);
+        });
+    });
+
+    router.get('/preregistrationFacade/upload', function(request, response) {
+
+        // validation
+        if (request.param('offset')) {
+            request.assert('offset').isInt();
+        }
+        if (request.param('limit')) {
+            request.assert('limit').isInt();
+        }
+
+        var errors = request.validationErrors();
+        if (errors) {
+            errorHandler.invalidRequest(response, error);
+            return;
+        }
+
+        // sanitize
+        var offset = 0;
+        if (request.param('offset')) {
+            request.sanitize('offset').toInt();
+            offset = request.param('offset')
+        }
+        var limit = 20;
+        if (request.param('limit')) {
+            request.sanitize('limit').toInt();
+            limit = request.param('limit')
+        }
+
+        preregistrationFacade.upload({
+            offset: offset,
+            limit: limit,
+            currentDatetime: request.currentDatetime
+        }, function(error, result) {
+            if (error) {
+                errorHandler.index(response, error);
+                return;
+            }
+            response.render('registration/index', result);
+        });
+    });
+}
